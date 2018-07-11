c63f3aeaa7ec2baeda93fd237f16cde6a7214f12
John Vincent <johnvuc@hotmail.com>
2018-07-11 17:58:57 +0200

    New React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Map from './components/Map.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
