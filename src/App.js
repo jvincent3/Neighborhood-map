69d984c8e980edb6f52b6cf17c4342eb110ccdff
Jvincent3 <johnvuc@hotmail.com>
2018-07-13 04:09:10 +0200

    Using new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Map from './components/Map.js';
+import Sidebar from './components/Sidebar.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map
+        center= {{ lat: 41.3851, lng: 2.1734 }}/>
+        <Sidebar />
       </div>
     );
   }
