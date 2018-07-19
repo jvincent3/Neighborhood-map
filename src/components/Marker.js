530060df5de0efcccade329d8887496a418d57fe
Jvincent3 <johnvuc@hotmail.com>
2018-07-19 03:50:06 +0200

    Adding Img inside the InfoWindow

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { Marker, InfoWindow} from "react-google-maps";
+import * as FourSquareAPI from '../api/FourSquareAPI';
 
 class MarkerComponent extends Component {
 
 	state = {
-		show: false
+		show: false,
+		photo: ''
 	}
 
 	onToggleOpen = () => {
 	      this.setState({
 	      	show: !this.state.show,
 	      })
+	}
+
+	componentDidMount = async() => {
+		const venueId = this.props.place.id;
+		const venuePhoto = await FourSquareAPI.getPhoto(venueId);
+
+		await this.setStateAsync({
+			photo: `${venuePhoto.prefix}300x300${venuePhoto.suffix}`
+		})
 	}
 
 	render() {
@@ -22,6 +33,7 @@ class MarkerComponent extends Component {
 							<div>
 								<h4>{this.props.place.name}</h4>
 								<p>{this.props.place.location.formattedAddress[0]}</p>
+								<img alt={`An image of ${this.props.place.name}`} src={this.state.photo ? this.state.photo : 'http://via.placeholder.com/300x300'}/>
 							</div>
 						</InfoWindow>}
 					</div>
