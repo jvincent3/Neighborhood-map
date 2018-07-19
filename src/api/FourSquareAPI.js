84a41e56c784d7ddfd65d7bb49366d05d020f721
Jvincent3 <johnvuc@hotmail.com>
2018-07-19 03:48:27 +0200

    Added getPhoto API request

--- src/api/FourSquareAPI.js
+++ src/api/FourSquareAPI.js
@@ -1,14 +1,18 @@
 import request from 'request';
 
+const api = 'https://api.foursquare.com/v2/venues/';
+const clientId = 'KS1YNFPQSMDAF4UGOGB5XJ4WU0YSDMCJRY2EAVZMFNDYNKI4';
+const clientSecret = 'NY5VL4R4CQXVHND1W0QSBWVJN3UZZFRUIZMWZECYMEP20MS5';
+
 export const getData = search => {
 	return new 	Promise(resolve => {
 
 		request({
-		  url: 'https://api.foursquare.com/v2/venues/search',
+		  url: `${api}search`,
 		  method: 'GET',
-		  qs: 	{
-		    client_id: 'KS1YNFPQSMDAF4UGOGB5XJ4WU0YSDMCJRY2EAVZMFNDYNKI4',
-		    client_secret: 'NY5VL4R4CQXVHND1W0QSBWVJN3UZZFRUIZMWZECYMEP20MS5',
+		  qs: {
+		    client_id: clientId,
+		    client_secret: clientSecret,
 		    ll: `${search.lat}, ${search.lng}`,
 		    query: 'food',
 		    v: '20180323',
@@ -18,9 +22,33 @@ export const getData = search => {
 		}, (err, res, body) => {
 			const data = JSON.parse(body);
 			if (Object.keys(data.response).length > 0) {
-					resolve(data.response.venues)
+				resolve(data.response.venues)
 			} else {
 				alert("The place you're current searching doesn't exist: " + search);
+			}
+		});
+	});
+}
+
+export const getPhoto = (venueId) => {
+	return new Promise (resolve => {
+		request({
+			url: `${api+venueId}/photos`,
+			method: 'GET',
+			qs: {
+				client_id: clientId,
+				client_secret: clientSecret,
+				v: '20180719',
+				height: '300',
+				width: '300',
+				limit: 1
+			}
+		}, (err, res, body) => {
+			const data = JSON.parse(body);
+			if (Object.keys(data.response).length > 0) {
+				resolve(data.response.photos.items);
+			} else {
+				console.log("The place you're current searching doesn't exist: ");
 			}
 		});
 	});
