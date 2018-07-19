a8436d7db4586fdeaa1e4672ce89b07ac4f1e5f4
Jvincent3 <johnvuc@hotmail.com>
2018-07-20 00:26:09 +0200

    Removed components. Added keys on Li's

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import SearchBox from './SearchBox';
 
 class Sidebar extends Component {
 	render () {
 		return (
 			<div id="breadcrumb" className="Sidebar">
-				<h2>Neighborhood Map</h2>
-				<SearchBox
-				updateCurrentLoc= {this.props.updateCurrentLoc}/>
 				<ul className="venuesList">
 					{
 						this.props.venues.map((place, index) => {
-							return <li onClick={() => {this.props.updateCenter(place)}}>{place.name}</li>
+							return <li key={index} onClick={() => {this.props.updateCenter(place)}}>{place.name}</li>
 						})
 					}
 				</ul>
