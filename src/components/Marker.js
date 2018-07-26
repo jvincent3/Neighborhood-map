a8fcf1d449a7b117f6bd360eeec7f22140483a35
Jvincent3 <johnvuc@hotmail.com>
2018-07-27 00:08:49 +0200

    Changed InfoWindow to InfoBox

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,39 +1,31 @@
 import React, { Component } from 'react';
-import { Marker, InfoWindow} from "react-google-maps";
+import { Marker } from "react-google-maps";
+import InfoBox from "react-google-maps/lib/components/addons/InfoBox";
 
 const google = window.google;
 
 class MarkerComponent extends Component {
 
 	state = {
-		show: false,
 		photo: ''
-	}
+	};
 
-	onToggleOpen = () => {
-	      this.setState({
-	      	show: !this.state.show,
-	      })
-	}
-	componentDidMount() {
-		console.log(this.props.focused)
-	}
 	render() {
 		return (
 			<div>
-				<Marker position={this.props.position} animation={google.maps.Animation.DROP} onClick={this.onToggleOpen}>
+				<Marker position={this.props.position} animation={this.props.place.verified ? google.maps.Animation.BOUNCE : null} onClick={() => this.props.toggleInfoBox(this.props.place.id)}>
 					<div>
-						{this.props.focused.focused && <InfoWindow onCloseClick={this.onToggleOpen}>
+						<InfoBox options={{pixelOffset: new google.maps.Size(50, -300) , closeBoxURL: '' }} visible={this.props.place.verified}>
 							<div>
 								<h4>{this.props.place.name}</h4>
 								<p>{this.props.place.location.formattedAddress[0]}</p>
 								<img alt={`${this.props.place.name}`} src={this.state.photo ? this.state.photo : 'http://via.placeholder.com/300x300'}/>
 							</div>
-						</InfoWindow>}
+						</InfoBox>
 					</div>
 				</Marker>
 			</div>
-		)
+			)
 	}
 }
 
